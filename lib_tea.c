e122b7e6e4baf6fd3cdf0e78adf43bc2fd0ab812
jeshiihu <jeshiihu@gmail.com>
2017-04-04 09:38:34 -0600

    think the tea interfacing is done..

--- lib_tea.c
+++ lib_tea.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include  "TEAEncryption.h"
 
+#include "encrypt.c"
+#include "decrypt.c"
+
 /*
  * Class:     TEAEncryption
  * Method:    encrypt
@@ -10,7 +13,23 @@
 JNIEXPORT void JNICALL Java_TEAEncryption_encrypt
   (JNIEnv *env, jobject object, jlongArray val, jlongArray key)
   {
+  	jboolean *is_copy = 0;
+	// jsize val_size = (*env)->GetArrayLength(env, val);
+	// jsize key_size = (*env)->GetArrayLength(env, key);
 
+	// ensure that isccopy is 0 so it will change it in memory
+  	jlong *val_prim = (jlong *) (*env)->GetIntArrayElements(env, val, is_copy);
+  	jlong *key_prim = (jlong *) (*env)->GetIntArrayElements(env, key, is_copy);
+
+  	if (val_prim == NULL || key_prim == NULL){
+    	printf("Cannot obtain val or key array from JVM\n");
+    	exit(-1);
+  	}
+
+  	// releasing the arrays copies back into memory
+  	encrypt(val_prim, key_prim);
+  	(*env)->ReleaseLongArrayElements(env, val, val_prim, 0);
+  	(*env)->ReleaseLongArrayElements(env, key, key_prim, 0);
   }
 /*
  * Class:     TEAEncryption
@@ -20,6 +39,22 @@ JNIEXPORT void JNICALL Java_TEAEncryption_encrypt
 JNIEXPORT void JNICALL Java_TEAEncryption_decrypt
   (JNIEnv *env, jobject object, jlongArray val, jlongArray key)
   {
+  	jboolean *is_copy = 0;
+	// jsize val_size = (*env)->GetArrayLength(env, val);
+	// jsize key_size = (*env)->GetArrayLength(env, key);
 
+	// ensure that isccopy is 0 so it will change it in memory
+  	jlong *val_prim = (jlong *) (*env)->GetIntArrayElements(env, val, is_copy);
+  	jlong *key_prim = (jlong *) (*env)->GetIntArrayElements(env, key, is_copy);
+
+  	if (val_prim == NULL || key_prim == NULL){
+    	printf("Cannot obtain val or key array from JVM\n");
+    	exit(-1);
+  	}
+
+  	// releasing the arrays copies back into memory
+  	decrypt(val_prim, key_prim);
+  	(*env)->ReleaseLongArrayElements(env, val, val_prim, 0);
+  	(*env)->ReleaseLongArrayElements(env, key, key_prim, 0);
   }
 
